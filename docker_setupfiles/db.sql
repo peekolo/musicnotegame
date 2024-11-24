9ec837d6726a8a8e3ddb69006bfabbc6135d590a
peekolo <peekolo@gmail.com>
2024-11-24 15:33:10 -0500

    remove cred

--- docker_setupfiles/db.sql
+++ docker_setupfiles/db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE musicnotegame;
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '47_911z8aWo<';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-CREATE USER 'musicnotegame'@'localhost' IDENTIFIED WITH mysql_native_password BY 'musicnotegamePass17!';
+CREATE USER 'musicnotegame'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
 GRANT ALL PRIVILEGES ON musicnotegame.* TO 'musicnotegame'@'localhost';
 FLUSH PRIVILEGES;
